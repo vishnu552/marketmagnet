b0447a1cefeb97c26349e721bdf25568ff1fc54c
vishnu suthar <sutharvishnu2021@gmail.com>
2023-12-24 21:03:43 +0530

    linking done

--- market-main/src/App.jsx
+++ market-main/src/App.jsx
@@ -4,10 +4,12 @@ import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer.jsx";
 import { AppProvider } from "./components/context.jsx";
 import Form from "./components/form.jsx";
+import ScrollToTop from "./components/ScrolltoTop.jsx";
 function App() {
   return (
     <>
       <AppProvider>
+        <ScrollToTop />
         <Navbar />
         <Outlet />
         <Footer />
