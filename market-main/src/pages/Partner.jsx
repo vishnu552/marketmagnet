1ad4714cb95a65e9e39cd6e3dba9e13c698c67d0
divyanshus020 <sharmadivyanshu281@gmail.com>
2023-12-24 19:01:11 +0530

    partner

--- market-main/src/pages/Partner.jsx
+++ market-main/src/pages/Partner.jsx
@@ -9,14 +9,14 @@ function Partner() {
 
           <div className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center bg-father">
 
-            <h1 className="text-white text-5xl mb-3">Welcome</h1>
+            <h1 className="text-[#3a41bd] text-bold text-5xl mb-3">Welcome</h1>
             <div>
-              <p className="text-white text-2xl">"We invite forward-thinking investors to join us on a journey of innovation and growth. By becoming our partner, you align yourself with a vision for success, where opportunities are explored, challenges are met with resilience, and together, we shape the future of HubAlt. Let's embark on this exciting venture together and redefine what's possible."</p>
+              <p className="text-bold text-white text-2xl">"We invite forward-thinking investors to join us on a journey of innovation and growth. By becoming our partner, you align yourself with a vision for success, where opportunities are explored, challenges are met with resilience, and together, we shape the future of HubAlt. Let's embark on this exciting venture together and redefine what's possible."</p>
             </div>
           </div>
           <div className="w-full lg:w-1/2 py-16 px-12">
-            <h2 className="text-3xl mb-4">Register</h2>
-            <p className="mb-4">
+            <h2 className="text-6xl text-bold text-center text-[#3a41bd] mb-4">Register</h2>
+            <p className="mb-4 text-bold text-center">
               Create your account. It’s free and only take a minute
             </p>
 
@@ -189,12 +189,12 @@ function Partner() {
               <div className="mt-6">
                 <input type="checkbox" className="border border-gray-400" />
                 <span>
-                  I accept the <a href="#" className="text-purple-500 font-semibold">Terms of Use</a> &  <a href="#" className="text-purple-500 font-semibold">Privacy Policy</a>
+                  I accept the <a href="#" className="text-[#3a41bd] font-semibold">Terms of Use</a> &  <a href="#" className="text-[#3a41bd] font-semibold">Privacy Policy</a>
                 </span>
               </div>
 
               <div className="mt-5">
-                <button className="w-full bg-purple-500 py-3 text-center text-white">Register Now</button>
+                <button className="w-full bg-[#3a41bd] py-3 text-center text-white">Register Now</button>
               </div>
             </form>
           </div>
