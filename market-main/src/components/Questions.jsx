49dbc2f4dd97e0df24651aee97eeb2e62e13f060
vishnu suthar <sutharvishnu2021@gmail.com>
2023-12-24 20:47:20 +0530

    hello

--- market-main/src/components/Questions.jsx
+++ market-main/src/components/Questions.jsx
@@ -5,10 +5,10 @@ function Questions({ questions, activeId, toggleQuestion }) {
   return (
     <section
       id="faq"
-      className="w-full mx-auto my-16 px-20 py-10 gap-12 bg-white"
+      className="w-full mx-auto my-24 px-20 py-10 gap-12 bg-white"
       data-aos="fade-up"
     >
-      <h3 className="mb-12 font-medium leading-5 text-6xl text-bold text-center text-[#3a41bd] tracking-wider">
+      <h3 className="mb-24 font-medium leading-5 text-6xl text-bold text-center text-[#3a41bd] tracking-wider">
         Frequently asked questions
       </h3>
       {questions.map((question) => {
